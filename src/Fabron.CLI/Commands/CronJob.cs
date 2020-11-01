259033d3f929194643992d24aedfe2a460bc641f
Sun Zhongfeng <suraciii@outlook.com>
2020-11-01 18:23:43 +0800

    wip

---
+++ src/Fabron.CLI/Commands/CronJob.cs
@@ -0,0 +1,13 @@
+﻿using System.CommandLine;
+
+namespace Fabron.CLI.Commands
+{
+    public class CronJob : Command
+    {
+        public CronJob()
+            : base("", "")
+        {
+        }
+
+    }
+}
