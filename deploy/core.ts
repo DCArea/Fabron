d39781970c77873b2e428eed480c68e3cf81da74
Sun Zhongfeng <csyszf@outlook.com>
2021-02-10 23:51:46 +0800

    configure pulumi pipeline

--- deploy/core.ts
+++ deploy/core.ts
@@ -3,25 +3,23 @@ import * as k8s from "@pulumi/kubernetes";
 import * as kx from "@pulumi/kubernetesx";
 const config = new pulumi.Config();
 
-export const namespace_fabron = new k8s.core.v1.Namespace("fabron");
+export const namespace = k8s.core.v1.Namespace.get("fabron", "namespace/fabron");
 
+const cr_pat = process.env["CR_PAT"];
+if (!cr_pat) { throw "missing CR_PAT"}
 export const secret_dca_regcred = new k8s.core.v1.Secret("dcaregcred", {
     metadata: {
-        namespace: namespace_fabron.metadata.name
+        namespace: namespace.metadata.name
     },
     type: "kubernetes.io/dockerconfigjson",
     stringData: {
-        ".dockerconfigjson": config
-            .requireSecret("ghcr_dca_password")
-            .apply(value => {
-                return JSON.stringify({
-                    auths: {
-                        "ghcr.io": {
-                            username: 'fabron',
-                            password: value
-                        }
-                    }
-                })
-            })
-    },
+        ".dockerconfigjson": JSON.stringify({
+            auths: {
+                "ghcr.io": {
+                    username: 'fabron',
+                    password: cr_pat
+                }
+            }
+        }),
+    }
 });
--- deploy/workload.ts
+++ deploy/workload.ts
@@ -1,10 +1,16 @@
-
 import * as k8s from "@pulumi/kubernetes";
 import * as kx from "@pulumi/kubernetesx";
+import { namespace } from "./core";
 
 const pb = new kx.PodBuilder({
     containers: [{
         image: "nginx",
-        ports: {http: 80}, // Simplified ports syntax.
-    }]
+        ports: { http: 80 }, // Simplified ports syntax.
+    }],
+});
+const deployment = new kx.Deployment("fabron-service", {
+    metadata: {
+        namespace: namespace.metadata.name
+    },
+    spec: pb.asDeploymentSpec({ replicas: 1 })
 });
