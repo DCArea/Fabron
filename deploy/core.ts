42a3ccee51a935627ec72e299110d128f5cd37ea
Sun Zhongfeng <csyszf@outlook.com>
2021-02-26 15:42:44 +0800

    Migrate pulumi backend to pulumi service (#13)
    * migrate backend from azblob to pulumi
    
    * fix --suppress-permalink
    
    * store ghcr pat as pulumi secret

--- deploy/core.ts
+++ deploy/core.ts
@@ -1,25 +1,29 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as k8s from "@pulumi/kubernetes";
 import * as kx from "@pulumi/kubernetesx";
-const config = new pulumi.Config();
+export const config = new pulumi.Config();
 
 export const namespace = k8s.core.v1.Namespace.get("fabron", "namespace/fabron");
 
-const cr_pat = process.env["CR_PAT"];
-if (!cr_pat) { throw "missing CR_PAT"}
+// const cr_pat = process.env["CR_PAT"];
+// if (!cr_pat) { throw "missing CR_PAT"}
+const cr_pat = config.requireSecret("cr_pat");
+const dockerconfigjson = cr_pat.apply(v =>
+    JSON.stringify({
+        auths: {
+            "ghcr.io": {
+                username: 'fabron',
+                password: v
+            }
+        }
+    })
+)
 export const secret_dca_regcred = new k8s.core.v1.Secret("dcaregcred", {
     metadata: {
         namespace: namespace.metadata.name
     },
     type: "kubernetes.io/dockerconfigjson",
     stringData: {
-        ".dockerconfigjson": JSON.stringify({
-            auths: {
-                "ghcr.io": {
-                    username: 'fabron',
-                    password: cr_pat
-                }
-            }
-        }),
+        ".dockerconfigjson": dockerconfigjson,
     }
 });
