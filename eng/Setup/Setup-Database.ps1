9a6deca722280984919b46383e0b28a245a57ef4
Sun Zhongfeng <suraciii@outlook.com>
2021-07-12 17:55:51 +0800

    remove setup scripts and load-testing scripts

--- eng/Setup/Setup-Database.ps1
+++
@@ -1,6 +0,0 @@
-
-Invoke-Sqlcmd -Query "CREATE DATABASE Fabron" -ServerInstance "(localdb)\MSSQLLocalDB"
-
-@("SQLServer-Main.sql", "SQLServer-Persistence.sql", "SQLServer-Reminders.sql") |% {
-    Invoke-Sqlcmd -InputFile "eng\Setup\$_" -ServerInstance "(localdb)\MSSQLLocalDB" -Database "Fabron"
-}
