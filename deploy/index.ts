ca3a173fe2c7b713af2d0c5b022d8c5bb829b062
Sun Zhongfeng <suraciii@outlook.com>
2021-07-20 17:14:42 +0800

    Deploy servicemonitor

--- deploy/index.ts
+++ deploy/index.ts
@@ -1,8 +1,11 @@
 import * as workloads from "./workloads";
 import * as redis from "./redis";
-import { namespace_name } from "./core";
+import * as monitoring from "./monitoring";
+import { namespace_name, service_name } from "./core";
 
 const redis_config = redis.deploy(namespace_name);
-const { deployment } = workloads.deploy(redis_config);
+const { deployment, service } = workloads.deploy(redis_config);
+
+monitoring.deploy(service_name, service);
 
 export const image = deployment.spec.template.spec.containers[0].image;
