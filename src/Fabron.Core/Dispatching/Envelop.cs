b7932d3c5d9c0f239cff42571b8bc0367ba8db54
Sun Zhongfeng <suraciii@outlook.com>
2023-03-30 15:03:24 +0800

    fix naming

--- src/Fabron.Core/Dispatching/Envelop.cs
+++ src/Fabron.Core/Dispatching/Envelop.cs
@@ -1,7 +1,6 @@
 ﻿using System.Text.Json;
 using System.Text.Json.Nodes;
 using System.Text.Json.Serialization;
-using Fabron.Models;
 
 namespace Fabron.Dispatching;
 
@@ -19,27 +18,3 @@ public record FireEnvelop(
     [JsonPropertyName("specversion")]
     public string SpecVersion { get; } = "1.0";
 };
-
-public static class EnvelopeExtensions
-{
-    public static FireEnvelop ToEnvelop(this Models.PeriodicTimer timer, DateTimeOffset schedule)
-    {
-        var source = $"periodic.fabron.io/{timer.Metadata.Key}";
-        var id = $"{source}/{schedule.ToUnixTimeSeconds()}";
-        return new(source, schedule, timer.Data, timer.Metadata.Extensions);
-    }
-
-    public static FireEnvelop ToEnvelop(this CronTimer timer, DateTimeOffset schedule)
-    {
-        var source = $"cron.fabron.io/{timer.Metadata.Key}";
-        var id = $"{source}/{schedule.ToUnixTimeSeconds()}";
-        return new(source, schedule, timer.Data, timer.Metadata.Extensions);
-    }
-
-    public static FireEnvelop ToEnvelop(this GenericTimer timer, DateTimeOffset schedule)
-    {
-        var source = $"timed.fabron.io/{timer.Metadata.Key}";
-        var id = $"{source}/{schedule.ToUnixTimeSeconds()}";
-        return new(source, schedule, timer.Data, timer.Metadata.Extensions);
-    }
-}
